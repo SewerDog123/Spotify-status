4bfd4ad168ee0dbab7225fe6f5a12f298e6398c3
SewerDog <nnthxa729@gmail.com>
2026-02-27 17:38:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 let currentSong = {
   user: null,
